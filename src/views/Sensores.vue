2c5275402e949ea91f89303902f40727b25e75b7
ablancaa <ablancaa@gmail.com>
2023-06-12 02:22:06 +0200

    mejoras css

--- src/views/Sensores.vue
+++ src/views/Sensores.vue
@@ -3,19 +3,20 @@
     <h1>Sensores de Sensitive Pot</h1>
         <div class="row">
                 <div class="col-md-4">
-                    <img src="../assets/sensores/humedadTierra.jpg" height="" class="polaroid" />
+                    <img src="../assets/sensores/humedadTierra.jpg" height="" class="polaroid" /> 
                 </div>
                 <div class="col-md-4">
                     <img src="../assets/sensores/todosloscomponentes.jpg" height="" class="polaroid" />
                 </div>
                 <div class="col-md-4">
-                    <img src="../assets/sensores/ESP8266-WeMos.webp" height="" class="polaroid" />
+                    <img src="../assets/sensores/ESP8266-WeMos.webp" height="150" class="polaroid" />
                 </div>
                 <div class="col-md-4">
                     <img src="../assets/sensores/placa-d1-mini-esp8266.jpg" height="" class="polaroid" />
                 </div>
             </div>
         </div>
+        <br/><br/>
 
 </template>
 
@@ -33,11 +34,12 @@ export default {
     margin-top: 60px;
 }
 .polaroid {
-  height: 270px;
+  height: 220px;
   background-color: white;
   padding: 10px 10px 50px 10px;
   border: 1px solid #bfbfbf;
   box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.8);
   margin-top: 30px;
 }
+
 </style>
