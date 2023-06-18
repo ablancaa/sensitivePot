878d014ac777bcbbf5d5896c7adac37fa21e25b4
ablancaa <ablancaa@gmail.com>
2023-06-18 22:17:04 +0200

    logo maceta

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,15 @@
 <template>
 <nav class="navbar navbar-dark bg-dark fixed-top">
   <div class="container-fluid">
-    <router-link to="/" class="nav-link active" aria-current="page"><a class="navbar-brand" href="#">Sensitive Pot </a></router-link>
+   
+    <router-link to="/" class="nav-link active" aria-current="page"><img src="../assets/icos/Planta.png" height="40"/> | <a class="navbar-brand" href="#">Sensitive Pot </a></router-link>
   
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Sensitive Pot Menú</h5>
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Sensitive Pot Menú</h5><img src="../assets/icos/Planta.png" height="40"/>
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       
